1be6e7d78b02c3488601ff32ed97c434c4b07b60
BhaveshBirla <bhaveshbirla1@gmail.com>
2025-07-26 15:27:09 +0530

    making responsive

--- frontend/src/components/admin/AdminJobs.jsx
+++ frontend/src/components/admin/AdminJobs.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../shared/Navbar'
 import { Input } from '../ui/input'
-import { Button } from '../ui/button' 
-import { useNavigate } from 'react-router-dom' 
-import { useDispatch } from 'react-redux' 
+import { Button } from '../ui/button'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import AdminJobsTable from './AdminJobsTable'
 import useGetAllAdminJobs from '@/hooks/useGetAllAdminJobs'
 import { setSearchJobByText } from '@/redux/jobSlice'
@@ -17,17 +17,23 @@ const AdminJobs = () => {
   useEffect(() => {
     dispatch(setSearchJobByText(input));
   }, [input]);
+
   return (
     <div>
       <Navbar />
-      <div className='max-w-6xl mx-auto my-10'>
-        <div className='flex items-center justify-between my-5'>
+      <div className='max-w-6xl mx-auto my-8 px-4 sm:px-6'>
+        <div className='flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between my-4'>
           <Input
-            className="w-fit"
+            className="w-full sm:w-1/2"
             placeholder="Filter by name, role"
             onChange={(e) => setInput(e.target.value)}
           />
-          <Button onClick={() => navigate("/admin/jobs/create")}>New Jobs</Button>
+          <Button
+            onClick={() => navigate("/admin/jobs/create")}
+            className="w-full sm:w-auto"
+          >
+            New Job
+          </Button>
         </div>
         <AdminJobsTable />
       </div>
